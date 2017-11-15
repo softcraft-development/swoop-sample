d20e820df54a74d7aac666fff8bc25fea6c0d7f9
Craig Walker <github@softcraft.ca>
2017-11-15 10:39:04 -0700

    Application comments

--- src/main/java/ca/softcraft/swoop/sample/Application.java
+++ src/main/java/ca/softcraft/swoop/sample/Application.java
@@ -3,10 +3,20 @@ package ca.softcraft.swoop.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The SwoopSample bootstrap system.
+ */
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * The application entry point.
+	 * @param args The command-line arguments.
+	 */
 	public static void main(String[] args) {
+		/**
+		 * I like how Spring Boot makes bootstrapping so clean.
+		 */
 		SpringApplication.run(Application.class, args);
 	}
 
