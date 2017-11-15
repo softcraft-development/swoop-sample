fccc26861c0cdee1eefc202948a360fdb46235f5
Craig Walker <github@softcraft.ca>
2017-11-15 10:03:09 -0700

    Call the Amdoren Time Zone API and return a NowResult

--- src/main/java/ca/softcraft/swoop/sample/controllers/time/NowController.java
+++ src/main/java/ca/softcraft/swoop/sample/controllers/time/NowController.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class NowController {
 	@GetMapping("/time/now")
 	NowResult get() {
-		return new NowResult(0, 0, 0, 0, 0, 0, 0);
+		/*
+		 * IMO, application-specific environment variables should be namespaced to avoid conflicts.
+		 * The exception being environment variables that are shared across applications.
+		 */
+		String apiKey = System.getenv("SWOOP_SAMPLE_AMDOREN_API_KEY");
+		if  (apiKey == null) {
+			throw new RuntimeException("Environment variable SWOOP_SAMPLE_AMDOREN_API_KEY is not defined.");
+		}
+		AmdorenTimeZoneResult current = AmdorenTimeZoneResult.load(apiKey);
+		return NowResult.fromTemporalAccessor(current.getTemporalAccessor(), current.getOffset() * 60);
 	}
 }
